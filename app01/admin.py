c05f771ce5d2ca6d741ff2dfba6f384f755a7d3c
KinneyGao <54998666+Kinnie-lemon@users.noreply.github.com>
2021-04-08 12:04:26 +0800

    Add files via upload

---
+++ app01/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from app01 import models
+# Register your models here.
+
+admin.site.register(models.Students)
+admin.site.register(models.Stu_detail)
+admin.site.register(models.Salers)
+admin.site.register(models.Teachers)
+admin.site.register(models.Classes)
+admin.site.register(models.Transcations)
+admin.site.register(models.Class_bkp)
+admin.site.register(models.Schedules)
+admin.site.register(models.Class_category)
+admin.site.register(models.Classrooms)
+admin.site.register(models.Outline)
+admin.site.register(models.Salary)
